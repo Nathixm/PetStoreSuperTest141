8ef94de3c581187e2b0237bfc2162fcaed25bb52
Nathália Martins <nath.html@gmail.com>
2024-08-06 21:37:30 -0300

    first commit

---
+++ test/api/pet.spec.js
@@ -0,0 +1,35 @@
+// Bibliotecas e Framework
+
+const supertest = require('supertest');
+const Test = require('supertest/lib/test');
+
+const petId = 297537601;
+
+
+// Em Js, clasee é opcional, mas pode agrupar em uma Describe
+
+describe('API petStore Swagguer - Pet', () => {
+
+// Atributos do grupo/describe
+
+const request = supertest('https://petstore.swagger.io/v2'); // Base url
+
+
+// Funções ou Métodos
+
+    it('POST Pet', () => {
+
+
+        
+// Atributos, Campos, Caracteristicas, Configurações
+
+// Funções de Apoio (opcional)
+
+
+ // Funções de Testes em Si
+
+
+    })
+
+});
+
