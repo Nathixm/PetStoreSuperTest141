0469c28aff8428e7a2bd8d0e8e4c1f3a73c2481e
Nathália Martins <nath.html@gmail.com>
2024-08-08 23:06:35 -0300

    Refatorando convertendo async-await, adicionando GET, PUT e DELETE

--- test/api/pet.spec.js
+++ test/api/pet.spec.js
@@ -1,34 +1,57 @@
-// Bibliotecas e Framework
 
 const supertest = require('supertest');
 const Test = require('supertest/lib/test');
 
 const petId = 297537601;
 
-
-// Em Js, classe é opcional, mas pode agrupar em uma Describe
-
 describe('API petStore Swagguer - Pet', () => {
 
-// Atributos do grupo/describe
+    const request = supertest('https://petstore.swagger.io/v2'); 
 
-const request = supertest('https://petstore.swagger.io/v2'); // Base url
+    const pet = require('../../vendors/json/pet.json');
 
+    it('POST Pet', async () => {
+        const res = await request
+            .post('/pet')
+            .send(pet);
 
-// Funções ou Métodos
+        expect(res.statusCode).toBe(200);
+        expect(res.body.id).toBe(petId);
+        expect(res.body.name).toBe('Jake');
+        expect(res.body.photoUrls[0]).toBe();
+        expect(res.body.category.name).toBe('dog');
+        expect(res.body.category.id).toBe(1);
+        expect(res.body.tags[0].name).toBe('vaccinated');
+        expect(res.body.tags[0].id).toBe(1);
+        expect(res.body.status).toBe('available');
+    });
+    
+    it('GET Pet', async () => {
+        const res = await request
+            .get(`/pet/${petId}`);
 
-    it('POST Pet', () => {
+        expect(res.statusCode).toBe(200);
+        expect(res.body.id).toBe(petId);
+        expect(res.body.status).toBe('available');
+    });    
 
+    it('PUT Pet', async () => {
+        const pet = require ('../../vendors/json/petput.json')
 
-        
-// Atributos, Campos, Caracteristicas, Configurações
+        const res = await request
+            .put(`/pet`)
+            .send(pet);
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.id).toEqual(petId);
+        expect(res.body.status).toEqual('sold');   
+    })
 
-// Funções de Apoio (opcional)
-
-// Funções de Testes em Si
-
-
+    it('DELETE Pet', async () => {
+        const res = await request
+            .delete(`/pet/${petId}`);
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.code).toEqual(200);
+        expect(res.body.message).toEqual(`${petId}`);
     })
 
 });
-
